bafdc4974997b13a9d9259dc3c5bfa1007923bee
Antonis Kalipetis <akalipetis@gmail.com>
2019-04-22 17:18:11 +0300

    Initial version 😱

---
+++ README.md
@@ -0,0 +1,36 @@
+# Docker Swarm DinD Operator
+
+Simple operator for running Docker in Docker (DinD) as a Swarm service
+
+## Configuration
+
+The following flags are currently available:
+
+* `--args string` - the arguments to give to the DinD container
+* `--binds stringArray` - the directories to bind in the container
+* `--image string` - the image to use for DinD (default "docker:18.09-dind")
+* `--name string` - the name to give to the DinD container (default "swarm-dind-operator")
+* `--stop-timeout int` - the timeout to wait when the container is stopped in seconds (default 10)
+* `--help` prints the help message
+
+## Example usage
+
+Given the following `docker-compose.yml` file
+
+```yaml
+version: '3.7'
+
+services:
+  dind:
+    image: sourcelair/swarm-operator-dind
+    command:
+    - swarm-operator-dind
+    - --args=--experimental
+    - --binds=/mnt/dind:/var/lib/docker
+```
+
+you can deploy the operator to Docker Swarm with
+
+```bash
+docker stack deploy -c docker-compose.yml
+```
---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	docker "github.com/fsouza/go-dockerclient"
+	"github.com/kballard/go-shellquote"
+	"github.com/spf13/pflag"
+
+	"github.com/pkg/errors"
+)
+
+var imageName, containerName, containerArgs string
+var containerStopTimeout int
+var containerBinds []string
+
+func main() {
+	os.Exit(runOperator())
+}
+
+func runOperator() int {
+	// parse arguments
+	pflag.StringVar(&imageName, "image", "docker:18.09-dind", "the image to use for DinD")
+	pflag.StringVar(&containerName, "name", "swarm-dind-operator", "the name to give to the DinD container")
+	pflag.StringVar(&containerArgs, "args", "", "the arguments to give to the DinD container")
+	pflag.IntVar(&containerStopTimeout, "stop-timeout", 10, "the timeout to wait when the container is stopped in seconds")
+	pflag.StringArrayVar(&containerBinds, "binds", []string{}, "the directories to bind in the container")
+	pflag.Parse()
+
+	args, err := shellquote.Split(containerArgs)
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "invalid arguments given"))
+		return 42
+	}
+
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "cannot create client"))
+		return 42
+	}
+	containers, err := client.ListContainers(docker.ListContainersOptions{
+		All: true,
+		Filters: map[string][]string{
+			"name": []string{containerName},
+		},
+	})
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "cannot list containers"))
+		return 42
+	}
+
+	for _, container := range containers {
+		if err := client.RemoveContainer(docker.RemoveContainerOptions{
+			Force: true,
+			ID:    container.ID,
+		}); err != nil {
+			fmt.Println(errors.Wrap(err, "cannot remove container"))
+			return 42
+		}
+	}
+	if _, err := client.InspectImage(imageName); err != nil {
+		if err != docker.ErrNoSuchImage {
+			fmt.Println(errors.Wrap(err, "cannot inspect image"))
+			return 42
+		}
+
+		if err := client.PullImage(docker.PullImageOptions{
+			Repository: imageName,
+		}, docker.AuthConfiguration{}); err != nil {
+			fmt.Println(errors.Wrap(err, "cannot pull image"))
+			return 42
+		}
+	}
+	container, err := client.CreateContainer(docker.CreateContainerOptions{
+		Name: containerName,
+		Config: &docker.Config{
+			Cmd:      args,
+			Hostname: containerName,
+			Image:    imageName,
+			Labels: map[string]string{
+				"com.sourcelair.swarm-dind-operator":      "true",
+				"com.sourcelair.swarm-dind-operator.name": containerName,
+			},
+			StopTimeout: containerStopTimeout,
+		},
+		HostConfig: &docker.HostConfig{
+			Privileged: true,
+			Binds:      containerBinds,
+		},
+	})
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "cannot create container"))
+		return 42
+	}
+
+	if err := client.StartContainer(container.ID, nil); err != nil {
+		fmt.Println(errors.Wrap(err, "cannot start container"))
+		return 42
+	}
+
+	ctx, done := context.WithCancel(context.Background())
+	defer done()
+
+	// forward container logs
+	go func() {
+		client.Logs(docker.LogsOptions{
+			Container:    container.ID,
+			Stdout:       true,
+			Stderr:       true,
+			OutputStream: os.Stdout,
+			ErrorStream:  os.Stderr,
+			Context:      ctx,
+			Follow:       true,
+		})
+	}()
+
+	defer client.RemoveContainer(docker.RemoveContainerOptions{
+		Force: true,
+		ID:    container.ID,
+	})
+
+	// wait for container to exit
+	containerExited := make(chan int, 1)
+	go func() {
+		exitCode, _ := client.WaitContainer(container.ID)
+		containerExited <- exitCode
+	}()
+
+	// catch signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs)
+
+	exitCode := 0
+
+	for {
+		select {
+		// propagate signals
+		case sig := <-sigs:
+			client.KillContainer(docker.KillContainerOptions{
+				ID:      container.ID,
+				Context: ctx,
+				Signal:  docker.Signal(sig.(syscall.Signal)),
+			})
+		// exit with same exit code
+		case exitCode = <-containerExited:
+			return exitCode
+		}
+	}
+}
