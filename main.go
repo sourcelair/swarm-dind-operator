9219bc97670aba944ddaf4fc1b2be4ef6ff7d670
Antonis Kalipetis <akalipetis@gmail.com>
2019-05-28 18:49:53 +0300

    Sync DiND and operator PID and Network namespaces

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	docker "github.com/fsouza/go-dockerclient"
@@ -36,6 +38,14 @@ func runOperator() int {
 		fmt.Println(errors.Wrap(err, "invalid arguments given"))
 		return 42
 	}
+
+	data, err := ioutil.ReadFile("/proc/self/cpuset")
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "cannot read cgroup file"))
+		return 42
+	}
+	parts := strings.Split(string(data), "/")
+	selfID := strings.TrimSpace(parts[len(parts)-1])
 
 	client, err := docker.NewClientFromEnv()
 	if err != nil {
@@ -78,9 +88,8 @@ func runOperator() int {
 	container, err := client.CreateContainer(docker.CreateContainerOptions{
 		Name: containerName,
 		Config: &docker.Config{
-			Cmd:      args,
-			Hostname: containerName,
-			Image:    imageName,
+			Cmd:   args,
+			Image: imageName,
 			Labels: map[string]string{
 				"com.sourcelair.swarm-dind-operator":      "true",
 				"com.sourcelair.swarm-dind-operator.name": containerName,
@@ -88,8 +97,10 @@ func runOperator() int {
 			StopTimeout: containerStopTimeout,
 		},
 		HostConfig: &docker.HostConfig{
-			Privileged: true,
-			Binds:      containerBinds,
+			Privileged:  true,
+			Binds:       containerBinds,
+			NetworkMode: fmt.Sprintf("container:%s", selfID),
+			PidMode:     fmt.Sprintf("container:%s", selfID),
 		},
 	})
 	if err != nil {
